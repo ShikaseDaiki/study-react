b24744ff5a850fa0898e69fdc4fac28bd3753ec6
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-06 11:09:07 +0900

    試しにクリックイベントを書いた。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,27 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useCallback } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e: any) => {
+    console.log(e.target.href);
+    e.preventDefasult();
+    alert(foo);
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header></Header>
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index"></Main>
     </div>
   );
