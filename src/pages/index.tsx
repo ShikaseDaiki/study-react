47b406b636cae64bba613fc4a1396ba31c39e606
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-06 13:23:02 +0900

    説明用のuseEffectを導入した。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,25 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const foo = 2;
+  // const foo = 2;
 
-  const handleClick = useCallback((e: any) => {
-    // console.log(e.target.href);
-    // e.preventDefasult();
-    // alert(foo);
+  // const handleClick = useCallback((e: any) => {
+  //   console.log(e.target.href);
+  //   e.preventDefasult();
+  //   alert(foo);
+  // }, []);
+
+  useEffect(() => {
+    console.log("マウント");
+    document.body.style.backgroundColor = "blue";
+    return () => {
+      console.log("アンマウント");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
