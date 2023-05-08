f9630607a7154c746d7ca558a8ec62a0675474f2
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-08 21:59:51 +0900

    APIを叩く処理を追加した。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,32 +2,39 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useCallback, useEffect, useState } from "react";
+
+type Post = {
+  id: number;
+  title: string;
+};
 
 const Home = (props: any) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
   return (
     <div>
       <Head>
         <title>Index Page</title>
       </Head>
-      <Header></Header>
-      <div className="text-red-600 flex flex-col items-center">
-        {props.isShow ? <h1>{props.count}</h1> : null}
-        <button onClick={props.handleClick}>ボタン</button>
-        <button onClick={props.handleDisplay}>
-          {props.isShow ? "非表示" : "表示"}
-        </button>
-      </div>
-
-      <div className="text-green-400 flex flex-col items-center">
-        <input type="text" value={props.text} onChange={props.handleChange} />
-        <button onClick={props.handleAdd}>追加</button>
-        <ul>
-          {props.array.map((item: string) => {
-            return <div key={item}>{item}</div>;
+      <Header />
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post: Post) => {
+            return <li key={post.id}>{post.title}</li>;
           })}
-        </ul>
-      </div>
-      <Main page="index"></Main>
+        </ol>
+      ) : null}
     </div>
   );
 };
