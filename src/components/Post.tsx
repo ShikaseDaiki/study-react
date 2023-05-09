bd1018b40a7e5c001f95e704e909f1d42f0264ee
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-09 21:17:24 +0900

    データ部分をコンポーネント化して、useStateを1つにまとめた。

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,10 +5,22 @@ type Post = {
   title: string;
 };
 
-export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<any>(null);
+type State = {
+  data: string[];
+  loading: boolean;
+  error: any;
+};
+
+export const Post = () => {
+  //   const [posts, setPosts] = useState([]);
+  //   const [loading, setLoading] = useState(true);
+  //   const [error, setError] = useState<any>(null);
+
+  const [state, setState] = useState<State>({
+    data: [],
+    loading: true,
+    error: null,
+  });
 
   const getPosts = useCallback(async () => {
     try {
@@ -17,34 +29,51 @@ export const Posts = () => {
         throw new Error("データの取得に失敗しました");
       }
       const json = await res.json();
-      setPosts(json);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          data: json,
+          loading: false,
+        };
+      });
     } catch (error) {
-      setError(error);
+      setState((prevState: any) => {
+        return {
+          ...prevState,
+          loading: false,
+          error: error,
+        };
+      });
     }
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  if (loading) {
+  if (state.loading === true) {
     <div>ローディング中です</div>;
   }
 
-  if (error) {
-    <div>{error.message}</div>;
+  if (state.error) {
+    <div>{state.error}</div>;
   }
 
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     <div>データは空です</div>;
   }
 
   return (
-    <ol>
-      {posts.map((post: Post) => {
-        return <li key={post.id}>{post.title}</li>;
-      })}
-    </ol>
+    <div>
+      {state.loading ? <div>ローディング中です</div> : null}
+      {state.error != null ? <div>エラーです</div> : null}
+      {state.data.length === 0 ? <div>データは空です</div> : null}
+
+      <ol>
+        {state.data.map((post: any) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ol>
+    </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,43 +3,16 @@ import Head from "next/head";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
 import { useCallback, useEffect, useState } from "react";
-import { Posts } from "@/components/Post";
+import { Post } from "@/components/Post";
 
-type Post = {
-  id: number;
-  title: string;
-};
-
-const Home = (props: any) => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<any>(null);
-
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("データの取得に失敗しました");
-      }
-      const json = await res.json();
-      setPosts(json);
-    } catch (error) {
-      setError(error);
-    }
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
+const Home = () => {
   return (
     <div>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Posts />
+      <Post />
     </div>
   );
 };
