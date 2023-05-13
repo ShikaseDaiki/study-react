36f0b96739990f9af00404dd057a966ca222e523
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-13 16:09:32 +0900

    投稿系は、postsにパスを変更した。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const NAV_ITEM = [
-  { href: "/", label: "Index" },
+  { href: "/posts", label: "Posts" },
   { href: "/about", label: "About" },
 ];
 
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -36,7 +36,7 @@ export const Posts = () => {
         {data?.map((post: any) => {
           return (
             <li key={post.id}>
-              <Link href={`/post/${post.id}`}>
+              <Link href={`/posts/${post.id}`}>
                 {post.id}. {post.title}
               </Link>
             </li>
