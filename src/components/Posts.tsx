5b4a3a25b1867c0d276292188adb1bd7ee913969
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-13 09:56:38 +0900

    ファイル名をPostからPostsに変更した。

---
+++ src/components/Posts.tsx
@@ -0,0 +1,48 @@
+import { usePosts } from "@/hooks/usePosts";
+import Link from "next/link";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+type State = {
+  data: string[];
+  loading: boolean;
+  error: any;
+};
+
+export const Posts = () => {
+  const { data, error, isLoading, isEmpty } = usePosts();
+
+  if (isLoading) {
+    <div>ローディング中です</div>;
+  }
+
+  if (error) {
+    <div>{error.message}</div>;
+  }
+
+  if (isEmpty) {
+    <div>データは空です</div>;
+  }
+
+  return (
+    <div>
+      {isLoading ? <div>ローディング中です</div> : null}
+      {error ? <div>{error.message}</div> : null}
+      {isEmpty ? <div>データは空です</div> : null}
+      <ol>
+        {data?.map((post: any) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/post/${post.id}`}>
+                {post.id}. {post.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+};
