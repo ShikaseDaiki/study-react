f098c1834c35fa3cc3301a2c55c5747b66af2840
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-13 16:16:49 +0900

    不要なこーどやファイルを削除した。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import "@/styles/globals.css";
-import { useCounter } from "@/hooks/useCounter";
-import { useInputArray } from "@/hooks/useInputArray";
-import { useBgColor } from "@/hooks/useBgColor";
 import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
-
-  return <Component {...pageProps} {...counter} {...inputArray} />;
+  return <Component {...pageProps} />;
 };
 
 export default App;
