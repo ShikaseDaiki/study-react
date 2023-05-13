10e60aeb5d93cec1ef627330c84d030041c8df03
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-13 16:14:33 +0900

    Indexから不要なコードを削除した。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,27 +7,11 @@ const About = (props: any) => {
   return (
     <div>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
-      <Header></Header>
-      <div className="text-red-600 flex flex-col items-center">
-        {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-        <button onClick={props.handleClick}>ボタン</button>
-        <button onClick={props.handleDisplay}>
-          {props.isShow ? "非表示" : "表示"}
-        </button>
-      </div>
-
-      <div className="text-green-400 flex flex-col items-center">
-        <input type="text" value={props.text} onChange={props.handleChange} />
-        <button onClick={props.handleAdd}>追加</button>
-        <ul>
-          {props.array.map((item: string) => {
-            return <div key={item}>{item}</div>;
-          })}
-        </ul>
-      </div>
-      <Main page="about"></Main>
+      <Header />
+      <h1>Next.jsで学ぶ React講座</h1>
+      <p>JSON PlaceholderのAPIを色々叩いてみる</p>
     </div>
   );
 };
