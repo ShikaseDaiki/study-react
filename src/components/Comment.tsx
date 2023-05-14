40de1de95f55bf3e4d9557c7804e9a598fce1ef8
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-14 19:58:54 +0900

    コメントの詳細ページを作成した。

---
+++ src/components/Comment.tsx
@@ -0,0 +1,33 @@
+import { useComment } from "@/hooks/useComment";
+import Head from "next/head";
+
+export const Comment = () => {
+  const { comment, error, isLoading } = useComment();
+
+  if (isLoading) {
+    <div>ローディング中です</div>;
+  }
+
+  if (error) {
+    <div>{error.message}</div>;
+  }
+
+  console.log({ comment, error, isLoading });
+
+  return (
+    <div>
+      {isLoading ? <div>ローディング中です</div> : null}
+      {error ? <div>{error.message}</div> : null}
+      {isLoading ? null : (
+        <div>
+          <Head>
+            <title>{comment?.body}</title>
+          </Head>
+          <h1>コメントID : {comment?.id}</h1>
+          <p>名前 : {comment?.name}</p>
+          {comment?.name ? <div>メールアドレス : {comment.email}</div> : null}
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/pages/comments/[id]/index.tsx
@@ -0,0 +1,13 @@
+import { Comment } from "@/components/Comment";
+import { Header } from "@/components/Header";
+
+const CommentsId = () => {
+  return (
+    <div>
+      <Header />
+      <Comment />
+    </div>
+  );
+};
+
+export default CommentsId;
