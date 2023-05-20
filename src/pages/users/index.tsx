ddd9db89a56499bd340c8d7e87b632fc03ed0c14
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-20 17:48:57 +0900

    ユーザー一覧にSSRを実装した。

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,15 +1,33 @@
 import Head from "next/head";
 import { Header } from "@/components/Header";
-import { Users as UsersComponent } from "@/components/Users";
+import { UsersComponent } from "@/components/Users";
+import { SWRConfig } from "swr";
 
-const Users = () => {
+export const getServerSideProps = async () => {
+  const USERS_API_URL = "https://jsonplaceholder.typicode.com/users";
+  const users = await fetch(USERS_API_URL);
+  const usersData = await users.json();
+
+  return {
+    props: {
+      fallback: {
+        [USERS_API_URL]: usersData,
+      },
+    },
+  };
+};
+
+const Users = (props: any) => {
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Users Page</title>
       </Head>
-      <Header />
-      <UsersComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UsersComponent />
+      </SWRConfig>
     </div>
   );
 };
