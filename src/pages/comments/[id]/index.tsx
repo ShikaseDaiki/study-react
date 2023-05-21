b768cd2dd444ceca217e7f2badda7052deb60ae4
ShikaseDaiki <daiki19977252@gmail.com>
2023-05-21 20:19:39 +0900

    コメント詳細ページをSGでレンダリングするよう変更した。

--- src/pages/comments/[id]/index.tsx
+++ src/pages/comments/[id]/index.tsx
@@ -1,11 +1,40 @@
-import { Comment } from "@/components/Comment";
+import { CommentComponent } from "@/components/Comment";
 import { Header } from "@/components/Header";
+import { SWRConfig } from "swr";
+import { idText } from "typescript";
 
-const CommentsId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment: any) => ({
+    params: { id: comment.id.toString() },
+  }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (ctx: any) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentsId = (props: any) => {
+  const { fallback } = props;
   return (
     <div>
       <Header />
-      <Comment />
+      <SWRConfig value={fallback}>
+        <CommentComponent />
+      </SWRConfig>
     </div>
   );
 };
